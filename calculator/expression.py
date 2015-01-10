cfb2d48b75d45682e9ed7231a1b6ee852a673f2e
Niko Wilbert <mail@nikowilbert.de>
2015-01-10 18:38:01 +0100

    improved calculator example

--- calculator/expression.py
+++ calculator/expression.py
@@ -3,33 +3,33 @@ Simple example with to model a multi-step process.
 """
 
 
-def expression():
+def expression_gen(nested=False):
     result = 0
     product = 1
     while True:
         value = yield 'number or ('
         if value == '(':
-            product *= yield from expression()
+            product *= yield from expression_gen(nested=True)
         else:
             try:
                 product *= int(value)
             except ValueError:
                 raise Exception('invalid input')
-        value = yield '+ or * or ) or ='
+        value = yield '+ or * or ' + (')' if nested else '=')
         if value == '+':
             result += product
             product = 1
         elif value == '*':
             pass
-        elif value == ')':
+        elif (nested and value == ')') or (not nested and value == '='):
             return result + product
         else:
             raise Exception('invalid input')
 
 
 if __name__ == '__main__':
-    print('enter ) at top level to terminate')
-    state = expression()
+    print('Welcome to the Calculator')
+    state = expression_gen()
     v = None
     while True:
         try:
