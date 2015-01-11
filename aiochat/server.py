427c9d543f2d91ed8439a68041bc510f7c26dcce
Niko Wilbert <mail@nikowilbert.de>
2015-01-11 14:51:59 +0100

    finished chat

--- aiochat/server.py
+++ aiochat/server.py
@@ -1,81 +1,98 @@
 """
-Simple Chat Server using asyncio, asyncio and Server Sent Events
+Simple Chat Server using asyncio, asyncio and Server Sent Events.
 """
 
 import asyncio
 import logging
 import sys
 import json
+import html
 from http.client import NO_CONTENT
 
+import netifaces
 from aiohttp import web
-
 
 log = logging.getLogger()
 
-next_message = None
+
+class ChatApp(web.Application):
+
+    def __init__(self, loop):
+        super().__init__(loop=loop)
+        self._next_message = asyncio.Future()
+        self.router.add_route('GET', '/chat', self._get_static_page)
+        self.router.add_route('POST', '/chat/messages/',
+                              self._post_chat_message.__get__(self))
+        self.router.add_route('GET', '/chat/messages/',
+                              self._get_chat_messages.__get__(self))
+
+    @staticmethod
+    def _get_static_page(_):
+        with open('client.html', 'rb') as fh:
+            return web.Response(body=fh.read())
 
 
-def get_static_page(_):
-    with open('client.html', 'rb') as fh:
-        return web.Response(body=fh.read())
+    @asyncio.coroutine
+    def _post_chat_message(self, request):
+        json_data = yield from request.json()
+        self._next_message.set_result({
+            'author': html.escape(json_data['username']),
+            'text': html.escape(json_data['text'])
+        })
+        self._next_message = asyncio.Future()
+        # content-type for https://bugzilla.mozilla.org/show_bug.cgi?id=521301
+        return web.Response(status=NO_CONTENT, content_type='text/html')
 
 
-@asyncio.coroutine
-def post_message(request):
-    global next_message
-    json_data = yield from request.json()
-    # TODO: bleach
-    next_message.set_result({
-        'author': json_data['username'],
-        'text': json_data['text']
-    })
-    next_message = asyncio.Future()
-    return web.Response(status=NO_CONTENT)
+    @asyncio.coroutine
+    def _get_chat_messages(self, request):
+        # see https://github.com/brutasse/asyncio-sse
+        response = web.StreamResponse()
+        response.headers.add('Content-Type', 'text/event-stream')
+        response.headers.add('Cache-Control', 'no-cache')
+        response.headers.add('Connection', 'keep-alive')
+        response.start(request)
+        response.write(b'retry: 1000\n')  # set retry interval to 1s
+        while True:
+            json_data = yield from asyncio.shield(self._next_message)
+            self._send_sse(json_data, response)
 
-
-@asyncio.coroutine
-def get_messages_sse(request):
-    # see https://github.com/brutasse/asyncio-sse
-    response = web.StreamResponse()
-    response.headers.add('Content-Type', 'text/event-stream')
-    response.headers.add('Cache-Control', 'no-cache')
-    response.headers.add('Connection', 'keep-alive')
-    response.start(request)
-
-    def send(json_data):
+    @staticmethod
+    def _send_sse(json_data, response):
         str_data = json.dumps(json_data)
         for line in str_data.split('\n'):
             response.write('data: {}\n'.format(line).encode('utf-8'))
+        response.write('id: fsd\n'.encode('utf-8'))
         response.write(b'\n')
 
-    while True:
-        data = yield from asyncio.shield(next_message)
-        send(data)
 
-
-def _get_app(loop):
-    global next_message
-    next_message = asyncio.Future()
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/chat', get_static_page)
-    app.router.add_route('POST', '/chat/messages/', post_message)
-    app.router.add_route('GET', '/chat/messages/sse', get_messages_sse)
-    return app
+def _print_server_info(port):
+    for name in netifaces.interfaces():
+        interface = netifaces.ifaddresses(name).get(netifaces.AF_INET)
+        if interface:
+            host = interface[0]['addr']
+            if host != '127.0.0.1':
+                print('Chat is served at:\n{}:{}/chat'.format(host, port))
 
 
 def main():
-    print('please open: localhost:8080/chat')
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    port = 8080
+    _print_server_info(port)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     loop = asyncio.get_event_loop()
-    app = _get_app(loop)
-    loop.run_until_complete(_start_app(loop, app, '127.0.0.1', 8080))
-    loop.run_forever()
+    app = ChatApp(loop)
 
+    @asyncio.coroutine
+    def _start_app(loop_, app_, host, port):
+        return (yield from loop_.create_server(app_.make_handler(), host, port))
 
-@asyncio.coroutine
-def _start_app(loop, app, host, port):
-    return (yield from loop.create_server(app.make_handler(), host, port))
+    loop.run_until_complete(_start_app(loop, app, '0.0.0.0', port))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
 
 
 if __name__ == '__main__':
