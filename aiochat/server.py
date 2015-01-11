7957db66b9641c562e914d4b65531cfb912aa5bd
Niko Wilbert <mail@nikowilbert.de>
2015-01-11 10:50:01 +0100

    sse is working

--- aiochat/server.py
+++ aiochat/server.py
@@ -5,7 +5,9 @@ Simple Chat Server using asyncio, asyncio and Server Sent Events
 import asyncio
 import logging
 import sys
+import json
 from http.client import NO_CONTENT
+
 from aiohttp import web
 
 
@@ -20,13 +22,43 @@ def get_static_page(_):
 @asyncio.coroutine
 def post_message(request):
     data = yield from request.json()
+    # TODO: bleach
     return web.Response(status=NO_CONTENT)
+
+
+@asyncio.coroutine
+def get_messages_sse(request):
+    # see http://aiohttp.readthedocs.org/en/latest/web.html?
+    response = web.StreamResponse()
+    response.headers.add('Content-Type', 'text/event-stream')
+    response.headers.add('Cache-Control', 'no-cache')
+    response.headers.add('Connection', 'keep-alive')
+    response.start(request)
+
+    def send(json_data):
+        str_data = json.dumps(json_data)
+        for line in str_data.split('\n'):
+            response.write('data: {}\n'.format(line).encode('utf-8'))
+        response.write(b'\n')
+
+    while True:
+        yield from asyncio.sleep(2)
+        send({
+            'author': 'Niko',
+            'text': 'Hallo'
+        })
+        yield from asyncio.sleep(2)
+        send({
+            'author': 'Miri',
+            'text': 'Bonjour'
+        })
 
 
 def _get_app(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/chat', get_static_page)
     app.router.add_route('POST', '/chat/messages/', post_message)
+    app.router.add_route('GET', '/chat/messages/sse', get_messages_sse)
     return app
 
 
